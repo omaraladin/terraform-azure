6c67cbb4451752cfb7b9e7637f1d5f91f3f84bd4
Abshnb <48786827+omaraladin@users.noreply.github.com>
2025-08-02 12:07:40 +0400

    Update vmss-main.tf

--- vmss/vmss-main.tf
+++ vmss/vmss-main.tf
@@ -1,23 +1,15 @@
 # Configure the AzureRM provider
-# This block specifies that Terraform will use the Azure Resource Manager (AzureRM) provider.
-# The 'features {}' block is often included to enable certain provider features,
-# though it's not strictly necessary for basic operations.
 provider "azurerm" {
   features {}
 }
 
 # Define a Resource Group
-# A Resource Group is a logical container for Azure resources.
-# All resources in this example will be deployed into this Resource Group.
 resource "azurerm_resource_group" "rg" {
   name     = "my-terraform-rg" # Name of the Resource Group
   location = "East US"         # Azure region where the Resource Group will be created
 }
 
 # Define a Virtual Network (VNet)
-# A VNet is the fundamental building block for your private network in Azure.
-# It allows many types of Azure resources to securely communicate with each other,
-# the internet, and on-premises networks.
 resource "azurerm_virtual_network" "vnet" {
   name                = "my-terraform-vnet"
   address_space       = ["10.0.0.0/16"] # The address space for the VNet
@@ -26,8 +18,6 @@ resource "azurerm_virtual_network" "vnet" {
 }
 
 # Define a Subnet
-# Subnets enable you to segment the virtual network into one or more sub-networks
-# and allocate a portion of the VNet's address space to each subnet.
 resource "azurerm_subnet" "subnet" {
   name                 = "my-terraform-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
@@ -36,8 +26,6 @@ resource "azurerm_subnet" "subnet" {
 }
 
 # Define a Network Security Group (NSG)
-# An NSG contains security rules that allow or deny inbound network traffic
-# to, or outbound network traffic from, several types of Azure resources.
 resource "azurerm_network_security_group" "nsg" {
   name                = "my-terraform-nsg"
   location            = azurerm_resource_group.rg.location
@@ -52,13 +40,12 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22" # SSH port
-    source_address_prefix      = "*"  # Allow from any IP address
+    source_address_prefix      = "187.86.25.145"  # Allow from this Specific IP address
     destination_address_prefix = "*"
   }
 }
 
 # Define a Public IP address
-# A Public IP address allows internet-facing communication to Azure resources.
 resource "azurerm_public_ip" "public_ip" {
   name                = "my-terraform-publicip"
   location            = azurerm_resource_group.rg.location
@@ -67,8 +54,6 @@ resource "azurerm_public_ip" "public_ip" {
 }
 
 # Define a Network Interface (NIC)
-# A NIC enables an Azure Virtual Machine to communicate with the internet,
-# Azure, and on-premises resources.
 resource "azurerm_network_interface" "nic" {
   name                = "my-terraform-nic"
   location            = azurerm_resource_group.rg.location
@@ -83,14 +68,12 @@ resource "azurerm_network_interface" "nic" {
 }
 
 # Associate the NSG with the Subnet
-# It's generally recommended to associate NSGs at the subnet level for broader control.
 resource "azurerm_subnet_network_security_group_association" "nsg_association" {
   subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
-# Define a Virtual Machine
-# This block creates an Ubuntu Server VM.
+# Define a Virtual Machine with Ubuntu OS
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "my-terraform-vm"
   location            = azurerm_resource_group.rg.location
@@ -99,7 +82,6 @@ resource "azurerm_linux_virtual_machine" "vm" {
   admin_username      = "azureuser"    # Administrator username for the VM
 
   # SSH Key for authentication (replace with your actual public key or generate one)
-  # For production, it's recommended to use a more secure method for key management.
   admin_ssh_key {
     username   = "azureuser"
     public_key = file("~/.ssh/id_rsa.pub") # Path to your SSH public key
@@ -119,17 +101,4 @@ resource "azurerm_linux_virtual_machine" "vm" {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS" # Standard Local Redundant Storage
   }
-}
-
-# Output the Public IP address of the VM
-# This output will display the public IP address after Terraform applies the configuration.
-output "public_ip_address" {
-  value       = azurerm_public_ip.public_ip.ip_address
-  description = "The public IP address of the Azure Virtual Machine."
-}
-
-# Output the SSH command
-output "ssh_command" {
-  value       = "ssh ${azurerm_linux_virtual_machine.vm.admin_username}@${azurerm_public_ip.public_ip.ip_address}"
-  description = "SSH command to connect to the Azure Virtual Machine."
 }
